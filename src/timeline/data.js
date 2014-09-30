59962f646a0305274b0eb968e08fec3cfac79792
Anthony Sennett <anthony.sennett@gmail.com>
2014-09-30 19:41:01 +0200

    sorting timeline stack with blurbs

--- src/timeline/data.js
+++ src/timeline/data.js
@@ -5,19 +5,22 @@ module.exports = {
         where: "University of Liverpool",
         start: "2004-09-01",
         end: "2006-07-01",
-        speil: null
+        speil: null,
+        images: ["http://placehold.it/100x100", "http://placehold.it/100x100"]
       }, {
         what: "BA in Music and Popular Music",
         where: "University of Liverpool",
         start: "2006-09-01",
         end: "2009-07-01",
-        speil: null
+        speil: null,
+        images: ["http://placehold.it/100x100", "http://placehold.it/100x100"]
       }, {
         what: "Web developer",
         where: "Dolphin Music",
         start: "2010-05-01",
         end: "2011-08-01",
-        speil: "Work included maintaining the customer-facing site (with close collaboration with the graphic-design team), working on backend systems, and maintaining existing integrations.  Worked with LAMP/MAMP, and HTML, CSS and JavaScript in a load-balanced environment.  Also provided technical support for the company's bespoke backend systems, including Netsuite, Exchange (later Google Apps) and the Avaya phone system.  Very much a one-man tech dept. position."
+        speil: "Work included maintaining the customer-facing site (with close collaboration with the graphic-design team), working on backend systems, and maintaining existing integrations.  Worked with LAMP/MAMP, and HTML, CSS and JavaScript in a load-balanced environment.  Also provided technical support for the company's bespoke backend systems, including Netsuite, Exchange (later Google Apps) and the Avaya phone system.  Very much a one-man tech dept. position.",
+        images: ["http://placehold.it/100x100", "http://placehold.it/100x100"]
       }, { 
         what: "Analyst Developer", 
         where: "Enlighten Designs", 
@@ -41,12 +44,14 @@ module.exports = {
         challenges that arise from distributed computing concerns; regular\
         meetings with business stakeholders to align goals and provide\
         support and technical perspective; maintaining the relationship\
-        between Enlighten and the company to foster future collaboration.\ </p>."
+        between Enlighten and the company to foster future collaboration.\ </p>.",
+        images: ["http://placehold.it/100x100", "http://placehold.it/100x100"]
       }, { 
         what: "JavaScript Arquitect", 
         where: "Typeform", 
         start: "2014-01-01", 
         end: null, 
-        speil: null
+        speil: null,
+        images: ["http://placehold.it/100x100", "http://placehold.it/100x100"]
       }]
 };
