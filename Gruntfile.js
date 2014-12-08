13db13934d537e4627fea8ec1fe424f5e4d19fed
Anthony Sennett <anthony.sennett@gmail.com>
2014-12-08 16:28:26 -0600

    switching to grunt-browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,50 +2,26 @@ module.exports = function(grunt) {
     
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        // browserify: {
-        //   dist: {
-        //     files: {
-        //       'build/app.js': 'src/**/*.js',
-        //     },
-        //     options: {
-        //       transform: ['node-underscorify', 'grunt-less-browserify']
-        //     }
-        //   }
-        // },
-        // watch: {
-        //   scripts: {
-        //     files: 'src/**/*',
-        //     tasks: 'default'
-        //   }
-        // },
-        
-        watchify: {
-          scripts: {
-            src: './src/app.js',
-            dest: 'build/app.js'
-          },
+		browserify: {
+			timeline: {
+				src: './src/app.js',
+				dest: 'build/app.js'
+			},
 			options:{
-				callback:function(browserify){
-					browserify.transform('node-lessify');
-					browserify.transform('node-underscorify');
-					return browserify;
-				}
+				watch:true,
+				keepAlive: true,
+				transform:['node-lessify', 'node-underscorify']
 			}
-        },
+		},
 		debug: {
 			options: {
 				open: false // do not open node-inspector in Chrome automatically
 			}
 		}
     });
-    
-    
-    // grunt.loadNpmTasks('grunt-browserify');
-    // grunt.loadNpmTasks('grunt-contrib-watch');
-    // grunt.registerTask('default', ['browserify']);
-    
-    grunt.loadNpmTasks('grunt-watchify');
-    grunt.registerTask('default', ['watchify']);
 
-	grunt.loadNpmTasks('grunt-debug-task');
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.registerTask('default', ['browserify']);
+
+	//grunt.loadNpmTasks('grunt-debug-task');
 };
